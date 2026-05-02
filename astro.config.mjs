6af6d4299a4c5f0e653e1f3cf954a5bcc979aadc
wilbrown_darkness <takoubrown@gmail.com>
2026-05-02 09:29:19 +0100

    chore: site URL prod (helia-52w.pages.dev)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  site: 'https://helia.local',
+  site: 'https://helia-52w.pages.dev',
   integrations: [
     starlight({
       title: 'Helia',
